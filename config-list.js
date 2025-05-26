e65226f198014148b4af98c56a998767929583fe
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-05-26 17:38:17 +0800

    西元2025年05月26日 (週一) 17時38分17秒 CST

--- config-list.js
+++ config-list.js
@@ -34,13 +34,19 @@ let feedList = [
 
 if (CONFIG.debug) {
   feedList = [
+    // {
+    //   title: 'IT咖啡馆',
+    //   feedID: 'it-coffee',
+    //   homepageURL: 'https://www.youtube.com/channel/UCXLV-KfDQAFUJ_as9H1Lfbw',
+    //   itemFilters: [
+    //     ItemFiltersPreset.between3minTo30Min,
+    //   ] // 霸道總裁攀岩開會、網紅天天搞小團體...《絕世網紅》劇情超浮誇？《 志祺今天不讀稿 》EP010｜志祺七七
+    // },
     {
-      title: 'IT咖啡馆',
-      feedID: 'it-coffee',
-      homepageURL: 'https://www.youtube.com/channel/UCXLV-KfDQAFUJ_as9H1Lfbw',
-      itemFilters: [
-        ItemFiltersPreset.between3minTo30Min,
-      ] // 霸道總裁攀岩開會、網紅天天搞小團體...《絕世網紅》劇情超浮誇？《 志祺今天不讀稿 》EP010｜志祺七七
+      title: '紫骷 玩遊戲',
+      feedID: 'Zikukirby',
+      homepageURL: 'https://www.youtube.com/channel/UCQceikp-aRr_e5uCARuPvEQ',
+      itemFilters: ItemFiltersPreset.between3minTo30Min,
     },
   ]
 }
